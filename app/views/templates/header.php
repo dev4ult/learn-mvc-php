1f4f9657315846c4c2ff13953031eb58d79d6565
dev4ult <nibrasbiasaaja@gmail.com>
2022-11-28 16:51:06 +0700

    template view setup

---
+++ app/views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="wireframe">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$data['page-title']?></title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
