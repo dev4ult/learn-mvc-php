7afc62705c5c49c45e6e5482b212928e43636349
dev4ult <nibrasbiasaaja@gmail.com>
2022-11-29 20:18:52 +0700

    config file for database and baseurl

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+const BASEURL = "http://localhost/FILE_PROJECT/learn-php-mvc/public";
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'phpdasar';
